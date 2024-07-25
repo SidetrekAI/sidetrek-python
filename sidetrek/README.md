6d19d7724add45ff8079f68b66a1e82e4aef4642
Seungchan Lee <seunggs@gmail.com>
2024-07-25 14:13:27 -0700

    fix exported file path to be relative to nb file path

--- sidetrek/README.md
+++ sidetrek/README.md
@@ -3,4 +3,5 @@
 ## Deployment
 
 0. Initial deployment only - configure pypi https://python-poetry.org/docs/repositories/#configuring-credentials
-1. `poetry publish --build`
+1. `poetry version <NEW_VERSION>`
+2. `poetry publish --build`
--- sidetrek/nbdev/export.py
+++ sidetrek/nbdev/export.py
@@ -1,3 +1,5 @@
+import os
+from pathlib import Path
 from nbdev.processors import NBProcessor  # type: ignore
 from nbdev.export import scrub_magics  # type: ignore
 from sidetrek.nbdev.directives import DbtMaker, ExportDBTProc
@@ -11,9 +13,11 @@ def nb_export_dbt(nb_path, procs=None, debug=False, dbt_maker=DbtMaker, name=Non
     Example usage:
     nb_export_dbt('path/to/notebook')
     """
+    nb_dirname = os.path.dirname(nb_path)
     exp = ExportDBTProc()
     nb = NBProcessor(nb_path, [exp] + L(procs) + L([scrub_magics]), debug=debug)
     nb.process()
     for filepath, cell in exp.file_content.items():
-        dm = dbt_maker(dest=filepath, nb_path=nb_path)
+        dest_filepath = str(Path(nb_dirname) / filepath)
+        dm = dbt_maker(dest=dest_filepath, nb_path=nb_path)
         dm.make(cell)
