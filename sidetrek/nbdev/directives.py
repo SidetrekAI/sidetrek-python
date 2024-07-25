95e8c17ef0cbc361310de370c88f826f72a5dd0e
Seungchan Lee <seunggs@gmail.com>
2024-07-24 19:12:53 -0700

    ready for release

--- sidetrek/nbdev/directives.py
+++ sidetrek/nbdev/directives.py
@@ -1,11 +1,9 @@
 import os
 from pathlib import Path
 from collections import defaultdict
-from fastcore.foundation import L # type: ignore
-from fastcore.basics import ifnone # type: ignore
-from nbdev.config import write_cells # type: ignore
-from nbdev.processors import NBProcessor # type: ignore
-from nbdev.export import scrub_magics # type: ignore
+from fastcore.foundation import L  # type: ignore
+from fastcore.basics import ifnone  # type: ignore
+from nbdev.config import write_cells  # type: ignore
 
 
 class DbtMaker:
@@ -24,21 +22,6 @@ class DbtMaker:
             cells = L([cell])
             write_cells(cells, self.hdr, f)
             f.write("\n")
-
-
-def nb_export_dbt(nb_path, procs=None, debug=False, dbt_maker=DbtMaker, name=None):
-    """
-    Create dbt files from notebook. Defaults to removing all magics from the cell.
-
-    Example usage:
-    nb_export_dbt('path/to/notebook')
-    """
-    exp = ExportDBTProc()
-    nb = NBProcessor(nb_path, [exp] + L(procs) + L([scrub_magics]), debug=debug)
-    nb.process()
-    for filepath, cell in exp.file_content.items():
-        dm = dbt_maker(dest=filepath, nb_path=nb_path)
-        dm.make(cell)
 
 
 class ExportDBTProc:
